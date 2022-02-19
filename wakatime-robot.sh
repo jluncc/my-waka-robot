dd18d241f368f1d88a9d07c53949915bcb173ce7
jluncc <cjljinglun@163.com>
2022-02-19 13:50:55 +0800

    添加企业微信机器人

--- wakatime-robot.sh
+++ wakatime-robot.sh
@@ -2,10 +2,12 @@
 
 range='Yesterday'
 
+# 1.获取waka数据
 waka_summaries_response=$(curl --location \
   --request GET "https://wakatime.com/api/v1/users/${waka_user_id}/summaries?range=${range}" \
   --header "Authorization: Bearer ${waka_access_token}")
 
+# 2.构造变量
 date=`echo "$waka_summaries_response" | jq '.data[0].range.date' | sed  's:":'':g'`
 echo $date
 
@@ -26,6 +28,7 @@ do
 done
 # echo $context_msg
 
+# 3.构造飞书curl数据体
 feishu_api_data='{
     "msg_type": "interactive",
     "card": {
@@ -82,13 +85,36 @@ feishu_api_data='{
         }
     }
 }'
-
 feishu_api_data=${feishu_api_data/'===date==='/${date}}
 feishu_api_data=${feishu_api_data/'===total_code_time==='/${total_code_time}}
 feishu_api_data=${feishu_api_data/'==context_msg=='/${context_msg}}
 # echo $feishu_api_data
 
-
+# 4.发送飞书消息
 curl --location --request POST "https://open.feishu.cn/open-apis/bot/v2/hook/${feishu_bot_token}" \
 --header 'Content-Type: application/json' \
 --data-raw "${feishu_api_data}"
+
+
+# 5.构造企业微信消息结构体
+qywxMessage='{
+    "msgtype": "markdown",
+    "markdown": {
+        "content": "
+            ## ===date=== 编程时间回顾\n 
+            ### 总耗时\n\n
+===total_code_time===\n\n
+            ### 各项目耗时\n
+            ==context_msg==\n
+            > [查看 waka time dashboard](https://wakatime.com/dashboard)"
+    }
+}'
+qywxMessage=${qywxMessage/'===date==='/${date}}
+qywxMessage=${qywxMessage/'===total_code_time==='/${total_code_time}}
+qywxMessage=${qywxMessage/'==context_msg=='/${context_msg}}
+# echo $qywxMessage
+
+# 6.发送企业微信消息
+curl --location --request POST "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${qywx_bot_token}" \
+--header 'Content-Type: application/json' \
+--data-raw "${qywxMessage}"
